76de746ab1b96cfd16a7c4004958bc5c06ed6194
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-02-14 03:36:34 -0700

    we up fr

--- hc-rt/xcrt/Phase0/Startup.cpp
+++ hc-rt/xcrt/Phase0/Startup.cpp
@@ -1,4 +1,4 @@
-//===- Startup.cpp --------------------------------------------------===//
+//===- Phase0/Startup.cpp -------------------------------------------===//
 //
 // Copyright (C) 2024 Eightfold
 //
@@ -13,10 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 //     limitations under the License.
-//
-//===----------------------------------------------------------------===//
-//
-//  Defines the entry functions used by MinGW.
 //
 //===----------------------------------------------------------------===//
 
