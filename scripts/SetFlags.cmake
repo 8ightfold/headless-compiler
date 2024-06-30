ed65449968d2a1dd0e6ab7988da4de4b53a30024
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-06-30 15:31:08 -0600

    Implement variadic target_internal_flags

--- scripts/SetFlags.cmake
+++ scripts/SetFlags.cmake
@@ -13,13 +13,52 @@ function(_hc_mutate_list ls fn)
   set(${ls} "${OUT_LIST}" PARENT_SCOPE)
 endfunction()
 
-function(_hc_make_iflag arg val)  
-  # TODO
-endfunction()
+##======================================================================##
+## target_internal_flags
+##======================================================================##
+
+macro(_hc_make_intflag arg flag)
+  if(${${flag}})
+    set(${arg} "-D_${flag}=1")
+  else()
+    if(NOT DEFINED ${flag})
+      message(WARNING "`${flag}` is NOT defined! Defaulting to OFF.")
+      # TODO: Set in parent scope?
+    endif()
+    set(${arg} "-D_${flag}=0")
+  endif()
+endmacro()
+
+macro(_hc_int_flags tgt vis ls)
+  if(DEFINED ${ls})
+    _hc_mutate_list(${ls} _hc_make_intflag)
+    target_compile_definitions(${tgt} ${vis} ${${ls}})
+  endif()
+endmacro()
 
 # Assign flags to a target. Converts NAME to _NAME.
 #  target_internal_flags(<target> <INTERFACE|PUBLIC|PRIVATE> [items...]...)
-function(target_internal_flags tgt vis flag)
+function(target_internal_flags tgt)
+  cmake_parse_arguments(X "" "" 
+    "PUBLIC;PRIVATE;INTERFACE" ${ARGN})
+  
+  get_target_property(type ${tgt} TYPE)
+  if(type STREQUAL "INTERFACE_LIBRARY")
+    if(DEFINED X_PUBLIC OR DEFINED X_PRIVATE)
+      message(SEND_ERROR "target_internal_flags may only "
+        "set INTERFACE properties on INTERFACE targets")
+      return()
+    endif()
+  endif()
+
+  _hc_int_flags(${tgt} PUBLIC X_PUBLIC)
+  _hc_int_flags(${tgt} PRIVATE X_PRIVATE)
+  _hc_int_flags(${tgt} INTERFACE X_INTERFACE)
+endfunction()
+
+# Assign flag to a target. Converts NAME to _NAME.
+#  target_internal_flags(<target> <INTERFACE|PUBLIC|PRIVATE> [item])
+function(target_internal_flags_ tgt vis flag)
   if(NOT DEFINED ${flag})
     message(WARNING "`${flag}` is NOT defined! Defaulting to OFF.")
     set(${flag} OFF PARENT_SCOPE)
@@ -30,6 +69,10 @@ function(target_internal_flags tgt vis flag)
     target_compile_definitions(${tgt} ${vis} "_${flag}=0")
   endif()
 endfunction()
+
+##======================================================================##
+## target_forward_options
+##======================================================================##
 
 macro(_hc_make_fwdarg arg val)
   set(${arg} "-D${val}=${${val}}")
