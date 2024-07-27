be95db986e52fbd420e3ff911d426e48d44e1e06
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-07-27 16:28:54 -0400

    Add example, callconv support

--- hc-rt/include/Meta/ASM.hpp
+++ hc-rt/include/Meta/ASM.hpp
@@ -25,6 +25,19 @@
 
 #include "Preproc.hpp"
 
+//////////////////////////////////////////////////////////////////////////
+// $ASM_alias(
+// int, (myfunc_a, _Z6myfuncif), (int x, float y));
+// 
+// $ASM_func(
+// int, myfunc, (int x, float y),
+//   "cvtsi2ss  %ecx,  %xmm0",
+//   "addss     %xmm0, %xmm1",
+//   "cvttss2si %xmm1, %eax",
+//   "retq"
+// )
+//////////////////////////////////////////////////////////////////////////
+
 #define $ASM_stmt(expr) expr ";\n"
 #define $ASM_block(exprs...) __asm__ volatile ( exprs );
 /// Creates an `__asm__` block with the input expressions, appends `";\n"`.
@@ -35,17 +48,52 @@
 #define $lbl(label) __hc_stringify(label:)
 
 /// Creates an assembly-only function.
-#define $ASM_func(ret, name, arglist, exprs...) \
+/// @param ret      The return type.
+/// @param callconv The calling convention.
+/// @param name     The name of the function.
+/// @param arglist  The list of parameters in parentheses.
+/// @param exprs    A comma-separated list of at&t assembly strings.
+#define $ASM_func_cc(ret, cconv, name, arglist, exprs...) \
 [[gnu::noinline, gnu::naked]] \
-ret __stdcall name arglist { \
+$PP_rm_parens(ret) cconv name arglist { \
   $ASM( exprs ) \
 }
+
+/// Creates an assembly-only function with the default calling convention.
+/// @param ret      The return type.
+/// @param name     The name of the function.
+/// @param arglist  The list of parameters in parentheses.
+/// @param exprs    A comma-separated list of at&t assembly strings.
+#define $ASM_func(ret, name, arglist, exprs...) \
+  $ASM_func_cc(ret, __defaultcall, name, arglist, ##exprs)
+
+//////////////////////////////////////////////////////////////////////////
 
 #define __$ASM_alias_alias(alias, _) alias
 #define __$ASM_alias_name(_, name) name
 #define __$ASM_alias_name_attr(_, name) \
   __attribute__((alias(__hc_stringify(name))))
+
+#define __$ASM_alias_def(alias_name, arglist) \
+  $PP_expand( __$ASM_alias_name  alias_name ) arglist
+#define __$ASM_alias_imp(alias_name, arglist) \
+  $PP_expand( __$ASM_alias_alias  alias_name ) arglist
+
+/// Creates a function alias.
+/// @param ret      The return type.
+/// @param callconv The calling convention of the functions.
+/// @param names    The names, in `(alias, real)` form.
+/// @param arglist  The list of parameters in parentheses.
+#define $ASM_alias_cc(ret, cconv, alias_name, arglist) \
+ extern "C" { [[gnu::noinline, gnu::naked]] $PP_rm_parens(ret) \
+  cconv __$ASM_alias_def(alias_name, arglist); } \
+ [[gnu::noinline, gnu::naked]] \
+ $PP_expand( __$ASM_alias_name_attr alias_name ) $PP_rm_parens(ret) \
+  cconv __$ASM_alias_imp(alias_name, arglist)
+
+/// Creates a function alias with the default calling convention.
+/// @param ret      The return type.
+/// @param names    The names, in `(alias, real)` form.
+/// @param arglist  The list of parameters in parentheses.
 #define $ASM_alias(ret, alias_name, arglist) \
- ret $PP_expand( __$ASM_alias_name  alias_name ) arglist; \
- $PP_expand( __$ASM_alias_name_attr alias_name ) \
- ret $PP_expand( __$ASM_alias_alias alias_name ) arglist
+  $ASM_alias_cc(ret, __defaultcall, alias_name, arglist)
