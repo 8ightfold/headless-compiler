837be5482f75bf67054cc2ef7b8f9b4d9262c7b0
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-07-27 22:53:36 -0400

    Setup NtRaiseException

--- hc-rt/src/Sys/Win/Nt/CheckPacking.cpp
+++ hc-rt/src/Sys/Win/Nt/CheckPacking.cpp
@@ -48,3 +48,54 @@ PACKING_TEST(ObjectID);
 PACKING_TEST(SectorSize);
 PACKING_TEST(Size);
 PACKING_TEST(Volume);
+
+#undef PACKING_TEST
+
+//////////////////////////////////////////////////////////////////////////
+
+#include "Except.hpp"
+
+#define XMM_TEST(offset, member) \
+ static_assert($offsetof(member, ContextXMMReg) == offset, \
+  "ContextXMMReg::" #member " is offset incorrectly. Please report this.")
+#define CTX_TEST(offset, member) \
+ static_assert($offsetof(member, ContextSave) == offset, \
+  "ContextSave::" #member " is offset incorrectly. Please report this.")
+
+static_assert(sizeof(ContextXMMReg) == 0x200);
+XMM_TEST(0x0,   header);
+XMM_TEST(0x20,  legacy);
+XMM_TEST(0xa0,  xmm0);
+XMM_TEST(0x190, xmm15);
+
+static_assert(sizeof(ContextSave) == 0x4d0);
+CTX_TEST(0x0,   p1h);
+CTX_TEST(0x8,   p2h);
+CTX_TEST(0x10,  p3h);
+CTX_TEST(0x18,  p4h);
+CTX_TEST(0x20,  p5h);
+CTX_TEST(0x28,  p6h);
+CTX_TEST(0x30,  ctx_flags);
+CTX_TEST(0x34,  mx_csr);
+CTX_TEST(0x38,  seg_cs);
+CTX_TEST(0x3a,  seg_ds);
+CTX_TEST(0x3c,  seg_es);
+CTX_TEST(0x3e,  seg_fs);
+CTX_TEST(0x40,  seg_gs);
+CTX_TEST(0x42,  seg_ss);
+CTX_TEST(0x44,  eflags);
+CTX_TEST(0x48,  dr0);
+CTX_TEST(0x50,  dr1);
+CTX_TEST(0x58,  dr2);
+CTX_TEST(0x60,  dr3);
+CTX_TEST(0x68,  dr6);
+CTX_TEST(0x70,  dr7);
+CTX_TEST(0x78,  GP);
+CTX_TEST(0x100, XMM);
+CTX_TEST(0x300, vec_register);
+CTX_TEST(0x4a0, vec_control);
+CTX_TEST(0x4a8, dbg_control);
+CTX_TEST(0x4b0, lbr_to_rip);
+CTX_TEST(0x4b8, lbr_from_rip);
+CTX_TEST(0x4c0, lex_to_rip);
+CTX_TEST(0x4c8, lex_from_rip);
---
+++ hc-rt/src/Sys/Win/Nt/Except.hpp
@@ -0,0 +1,80 @@
+//===- Sys/Win/Nt/Except.hpp ----------------------------------------===//
+//
+// Copyright (C) 2024 Eightfold
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//===----------------------------------------------------------------===//
+
+#pragma once
+
+#include <Common/PadStruct.hpp>
+#include "Generic.hpp"
+
+namespace hc::sys::win {
+  struct Reg128 {
+    u64 low;
+    i64 high;
+  };
+
+  struct ContextGPReg {
+    u64 rax, rcx, rdx, rbx, rsp, rbp;
+    u64 rsi, rdi;
+    u64 r8, r9, r10, r11, r12, r13, r14, r15;
+    u64 rip;
+  };
+
+  struct _ContextXMMReg {
+    Reg128 header[2];
+    Reg128 legacy[8];
+    Reg128 xmm0,  xmm1,  xmm2,  xmm3;
+    Reg128 xmm4,  xmm5,  xmm6,  xmm7;
+    Reg128 xmm8,  xmm9,  xmm10, xmm11;
+    Reg128 xmm12, xmm13, xmm14, xmm15;
+  };
+
+  using ContextXMMReg = com::PadStruct<_ContextXMMReg, 0x200>;
+  static_assert(sizeof(ContextXMMReg) == 0x200);
+
+  ////////////////////////////////////////////////////////////////////////
+
+  struct ExceptionRecord {
+  public:
+    i32               code;
+    u32               flags;
+    ExceptionRecord*  record;
+    void*             address;
+    u32               nparams;
+    u64               info[15];
+  }; 
+
+  struct ContextSave {
+    static void Capture(ContextSave* ctx);
+  public:
+    u64           p1h, p2h, p3h, p4h, p5h, p6h;
+    u32           ctx_flags;
+    u32           mx_csr;
+    u16           seg_cs, seg_ds, seg_es, seg_fs, seg_gs, seg_ss;
+    u32           eflags;
+    u64           dr0, dr1, dr2, dr3, dr6, dr7;
+    ContextGPReg  GP;
+    ContextXMMReg XMM;
+    Reg128        vec_register[26];
+    u64           vec_control;
+    u64           dbg_control;
+    u64           lbr_to_rip;   // Last branch to RIP
+    u64           lbr_from_rip; // Last branch from RIP
+    u64           lex_to_rip;   // Last exception to RIP
+    u64           lex_from_rip; // Last exception from RIP
+  };
+} // namespace hc::sys::win
